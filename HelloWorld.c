36d0e0033b421b529ad8fa15c129a842951ae689
Yamuna M <114819267+myamuna1406@users.noreply.github.com>
2023-10-26 08:21:43 +0530

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,5 @@
+#include <stdio.h>//includes the standard input/output library
+int main(){//Its main function, serves as the entry point of the program
+    printf("Hello World!");// function to print the text (The text is enclosed in double quotes, and it will be displayed as it is)
+    return 0;//a return value of 0 from the main function conventionally indicates that the program has executed without errors
+}
